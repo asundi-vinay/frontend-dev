e7ff7a9c9a846d9b6b3488c8095dfd9a7fb6af0f
unknown <vinayasundi111@gmail.com>
2025-01-18 21:44:37 +0530

    aws1

--- aws1.py
+++ aws1.py
@@ -1,13 +1,17 @@
 import json
-
-def add_num(number,sum):
-    n1=number.get('n1')
-    n2=number.get('n2')
-    result=n1+n2
-    return{
-        'statusCode':200,
-        'body':json.dumps({'res':result})
-        
-    }
+def add_num(number, sum):
+    try:
+        n1 = number.get('n1', 0)
+        n2 = number.get('n2', 0)
+        result = n1 + n2
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'result': result})
+        }
+    except TypeError:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'Invalid input'})
+        }
 
 
